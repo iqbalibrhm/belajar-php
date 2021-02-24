43f9ccbcc98cbeb3c46d627cb20e1d8adf095445
iqbalibrhm <ibrahimiqbal535@gmail.com>
2021-02-24 15:32:49 +0700

    belajar php dasar

---
+++ dasar.php
@@ -0,0 +1,28 @@
+<?php
+
+    define("PHI",3.14);
+
+    $jari_jari = 11;
+    $luas_lingkaran = PHI * $jari_jari * $jari_jari;
+    echo $luas_lingkaran;
+
+    $nama_lengkap = "Iqbal Ibrahim";
+    $angka = 1;
+    $nama = "iqbal";
+    $angka_desimal = 0.25;
+    echo $angka;
+    echo "</br>";
+    echo $nama;
+    echo "</br>";
+    echo $angka_desimal;
+    echo "</br>";
+    echo $nama_lengkap;
+
+
+    echo "</br>";
+    echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+
+    echo "</br>";
+    echo "nama saya : ".$nama_lengkap;
+
+?>
